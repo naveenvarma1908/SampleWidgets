96bf78a29b30307d7b264eae08a8508cc9436932
Naveen Varma <naveenvarmatrio@gmail.com>
2017-11-11 08:41:33 +0530

    Bug Fixed for Widgets

--- app/src/main/java/com/naveen/samplewidgets/MainActivity.java
+++ app/src/main/java/com/naveen/samplewidgets/MainActivity.java
@@ -16,7 +16,6 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Declaration of Widgets
     private ToggleButton toggleButton;
     private Switch aSwitch;
     private SwitchCompat switchCompat;
@@ -24,17 +23,18 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout checkLayout,radioLayout;
     private CheckBox cri,foot,volley;
     private RadioGroup radioGroup;
-    private Button bn_check,bn_radio;
+    private Button bn_radio,bn_check;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Connecting the Widgets with View by using FindViewByID
+        //Connecting Backend to View by using findViewByID
 
         toggleButton = (ToggleButton)findViewById(R.id.toggleButton);
-        aSwitch = (Switch) findViewById(R.id.switch_wifi);
+        aSwitch =(Switch)findViewById(R.id.switch_wifi);
         switchCompat = (SwitchCompat)findViewById(R.id.cswitch);
 
         checkLayout = (LinearLayout)findViewById(R.id.checkLayout);
@@ -44,48 +44,60 @@ public class MainActivity extends AppCompatActivity {
         foot = (CheckBox)findViewById(R.id.check__foot);
         volley = (CheckBox)findViewById(R.id.check__volley);
 
+        bn_check = (Button)findViewById(R.id.bn_check_trigger);
+        bn_radio = (Button)findViewById(R.id.bn_radio_trigger);
+
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
 
-        bn_check =(Button)findViewById(R.id.bn_check_trigger);
-        bn_radio =(Button)findViewById(R.id.bn_radio_trigger);
+
+        //By Default Switch & Switch Compat should be disabled & Non Clickable
+
+        aSwitch.setClickable(false);
+        switchCompat.setClickable(false);
+
+        aSwitch.setEnabled(false);
+        switchCompat.setEnabled(false);
 
 
 
-        //Disabling the Widgets by there properties
-            aSwitch.setClickable(false);
-            aSwitch.setEnabled(false);
-
-            switchCompat.setClickable(false);
-            switchCompat.setEnabled(false);
-
+        //Event on Widgets
         toggleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                   //Controlling the ToggleButton
-                if (toggleButton.isChecked()){//true
+                //Actions
 
-                   //Turn on the click option on widgets.
+                if (toggleButton.isChecked()){ //true
+                    //Changing color if ToggleButton state is True to Yellow
+                    //toggleButton.setBackgroundColor(Color.YELLOW);
+
+                    //User should be able to click on
+                    // switch & switchcompat after turning ON toggleButton
 
                     aSwitch.setEnabled(true);
                     aSwitch.setClickable(true);
+
                     switchCompat.setEnabled(true);
                     switchCompat.setClickable(true);
 
-                    //performing click event on widgets
+                    //Actions to be performed after widgets are active.
+
                     aSwitch.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
 
-                            //Turned ON then visible the check layout,
-                            //Remove Visibility of RadioLayout & SwitchCompat(Gender)
+                            if (aSwitch.isChecked()){
 
-                            if (aSwitch.isChecked()){ //true
-
+                                //Visible the checkLayout which contains sports.
                                 checkLayout.setVisibility(View.VISIBLE);
-                                radioLayout.setVisibility(View.GONE);
+                                //Remove visibility of switchCompat.
                                 switchCompat.setVisibility(View.GONE);
+                                //Remove visibility of radioLayout.
+                                radioLayout.setVisibility(View.GONE);
 
+
+                                //After check layout is visible performing
+                                // action on the button and taking inputs from the user.
                                 bn_check.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View v) {
@@ -93,38 +105,44 @@ public class MainActivity extends AppCompatActivity {
                                         StringBuilder stringBuilder = new StringBuilder();
                                         stringBuilder.append("Selected Sports: ").append("\n");
 
-                                        if (cri.isChecked()){
+                                        if (cri.isChecked()){//true
                                             stringBuilder.append(cri.getText().toString()).append("\n");
                                         }
 
-                                        if (foot.isChecked()){
+                                        if (foot.isChecked()){//true
                                             stringBuilder.append(foot.getText().toString()).append("\n");
                                         }
 
-                                        if (volley.isChecked()){
-
-                                            stringBuilder.append(volley.getText().toString());
+                                        if (volley.isChecked()){//true
+                                            stringBuilder.append(volley.getText().toString()).append("\n");
                                         }
 
-
-                                        //Show the selected Sports using Toasting
+                                        //Using Toast we are displaying data.
                                         Toast.makeText(MainActivity.this, stringBuilder.toString(), Toast.LENGTH_SHORT).show();
 
-                                        //Remove Visibility of current layout.
                                         checkLayout.setVisibility(View.GONE);
-                                        //Enable Visibility of SwitchCompat
+
                                         switchCompat.setVisibility(View.VISIBLE);
+
+                                        aSwitch.setChecked(false);
+
 
                                     }
                                 });
 
 
-                            }else {
 
-                                checkLayout.setVisibility(View.GONE);
+
+                            }else{
+
+                                //InVisible the checkLayout which contains sports.
+                                checkLayout.setVisibility(View.INVISIBLE);
+                                //visibility of switchCompat.
                                 switchCompat.setVisibility(View.VISIBLE);
                                 aSwitch.setChecked(false);
+
                             }
+
                         }
                     });
 
@@ -132,52 +150,109 @@ public class MainActivity extends AppCompatActivity {
                     switchCompat.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            if (switchCompat.isChecked()){//True
 
+
+                            if (switchCompat.isChecked()){
+
+                                //Visible the checkLayout which contains sports.
                                 radioLayout.setVisibility(View.VISIBLE);
+                                //Remove visibility of switchCompat.
                                 aSwitch.setVisibility(View.GONE);
+                                //Remove visibility of radioLayout.
                                 checkLayout.setVisibility(View.GONE);
+
 
                                 bn_radio.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View v) {
 
-                                        int selected = radioGroup.getCheckedRadioButtonId();
+                                        //Parsing selected RadioButton ID into integer Selected.
 
+                                        int selected = radioGroup.getCheckedRadioButtonId();
 
                                         RadioButton radioButton = (RadioButton)findViewById(selected);
 
-                                        Toast.makeText(MainActivity.this, "Selected Gender: \n" +radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(MainActivity.this, "Selected Gender: " +radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
 
 
                                         radioLayout.setVisibility(View.GONE);
                                         aSwitch.setVisibility(View.VISIBLE);
-
+                                        switchCompat.setChecked(false);
                                     }
                                 });
 
-                            }else {
 
-                                radioLayout.setVisibility(View.GONE);
+                            }else{
+
+                                //InVisible the checkLayout which contains sports.
+                                radioLayout.setVisibility(View.INVISIBLE);
+                                //visibility of switchCompat.
                                 aSwitch.setVisibility(View.VISIBLE);
                                 switchCompat.setChecked(false);
+
                             }
+
+
                         }
                     });
 
-                }else { //false
 
-                    aSwitch.setClickable(false);
-                    aSwitch.setEnabled(false);
+                }else{//false
+                    //Changing color if ToggleButton state is False to RED
+                    /*toggleButton.setBackgroundColor(Color.RED);*/
+
                     aSwitch.setChecked(false);
-                    switchCompat.setClickable(false);
-                    switchCompat.setEnabled(false);
                     switchCompat.setChecked(false);
 
+                    aSwitch.setEnabled(false);
+                    switchCompat.setEnabled(false);
+
+                    checkLayout.setVisibility(View.GONE);
+                    radioLayout.setVisibility(View.GONE);
+
+                    aSwitch.setVisibility(View.VISIBLE);
+                    switchCompat.setVisibility(View.VISIBLE);
 
                 }
+
             }
         });
+/*
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Actions
+
+                if (aSwitch.isChecked()){ //true
+                    //Changing TEXT if it turned ON
+                    aSwitch.setText("WIFI ON");
+                }else {//false
+                    //Changing TEXT if it turned OFF
+                    aSwitch.setText("WIFI OFF");
+                }
+
+
+            }
+        });*/
+
+
+      /*  switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (switchCompat.isChecked()){ //true
+                    //Changing TEXT if it turned ON
+                    switchCompat.setText("WIFI ON");
+                }else {//false
+                    //Changing TEXT if it turned OFF
+                    switchCompat.setText("WIFI OFF");
+                }
+            }
+        });*/
+
+
+
 
     }
 }
